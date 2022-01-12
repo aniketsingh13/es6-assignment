7b7d285be52b1ab883a3ed66969638ba40ad74bf
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-01-13 00:45:16 +0530

    first question section from the assignment

---
+++ question1/app.js
@@ -0,0 +1,35 @@
+//a
+/*  const aloo = 1;
+if(aloo===1){
+    let a=2;
+    console.log(a);
+}
+console.log(aloo);  */
+
+//b
+// const multiply=(x,y)=>x*y;
+
+//c
+/* const customer = {
+    names: "Bhaalo"
+  };
+  const card = {
+    amount: 20,
+    product: "Aaalo",
+    unitprice: 50
+  };
+
+let {names}=customer;
+let{amount,product,unitprice}=card;
+let message = `Hello   ${names}  wants to buy  ${amount}  ${product}  for price of card   
+${unitprice} per piece`
+
+console.log(message) */
+
+//d
+// const Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"]
+// const [ceo,rocketceo]=Neog;
+// console.log(ceo);
+// console.log(rocketceo);
+
+//e
